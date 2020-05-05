69f5580976c929c93cda5b4da8b87e1e05ee93fc
Ben Drawer <benjamin.drawer@tray.io>
2020-05-05 18:15:35 +0100

    Update divelog-server-utils

--- src/server.js
+++ src/server.js
@@ -3,8 +3,8 @@ const { importSchema } = require("graphql-import");
 const { makeExecutableSchema } = require("graphql-tools");
 const DataLoader = require("dataloader");
 const { RedisPubSub } = require("graphql-redis-subscriptions");
-const { authUtils, redisClient } = require("@btdrawer/divelog-server-utils");
-const { getUserId } = authUtils;
+const { auth, redisClient } = require("@btdrawer/divelog-server-utils");
+const { getUserId } = auth;
 
 const Query = require("./resolvers/Query");
 const Mutation = require("./resolvers/Mutation");
